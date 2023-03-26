c41e41b07fc12b41eb535d46ed2c169363484337
Bilalzaiter <91758174+Bilalzaiter@users.noreply.github.com>
2023-03-26 18:44:37 +0300

    Updates

--- src/components/BackgroundImg/BackgroundImg.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './BackgroundImg.css'
-
-const BackgroundImg = () => {
-  return (
-    <div className="background-image"></div>
-  )
-}
-export default BackgroundImg;
